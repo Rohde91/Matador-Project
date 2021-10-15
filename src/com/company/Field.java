5eeaf4cdf52a702a5492fc5383f83208346187ef
Martin Rohde <mart3b24@edu.easj.dk>
2021-10-15 20:35:23 +0200

    Complete project
    Can be made more, but very good

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class Field {
-    String place;
+    static int[] place = new int[10];
+
     int price;
 
     // Feldt navn
-    static String[] field = {"Rådhuspladsen", "Åkandevej", "Bistandskontoret", "Ved  postkassen", "András hjem", "Det lokale bodega", "AA mødet", "Field of dreams", "Strandet", "Java district"};
+    static String[] field = {"Rådhuspladsen(START)", "Åkandevej", "Bistandskontoret", "Ved postkassen", "András hjem", "Det lokale bodega", "AA mødet", "Field of dreams", "Strandet", "Java district (MÅL)"};
     // Feldt pris
     static int[] fieldPrice = {100, 100, 100, 100, 10, 100, 100, 10000, 100, 100};
-    static int[] owner = new int[3];
-
+    static int[] owner = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +9,7 @@ public class Main {
         User user1 = new User();
         User user2 = new User();
         User user3 = new User();
-        //AllUsers All = new AllUsers();
+        AllUsers All = new AllUsers();
 
         AllUsers.userListe[0] = user1;
         AllUsers.userListe[1] = user2;
@@ -27,13 +26,15 @@ public class Main {
         System.out.print("Username Player 3: ");
         AllUsers.userListe[2].navn = scanner.nextLine();
 
+        Board.playersNames[0]= String.valueOf(AllUsers.userListe[0]);
+        Board.playersNames[1]= String.valueOf(AllUsers.userListe[1]);
+        Board.playersNames[2]= String.valueOf(AllUsers.userListe[2]);
+
 
         System.out.println("Player 1 is: " + AllUsers.userListe[0]);
         System.out.println("Player 2 is: " + AllUsers.userListe[1]);
         System.out.println("Player 3 is: " + AllUsers.userListe[2]);
-        System.out.println(AllUsers.userListe[0]);
-        Bank.Purchase(0,1);
 
-        Dice.RollResult(1);
+        Board.Start();
     }
 }
